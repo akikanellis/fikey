84101405eb7029a146f46959a50be149e74daf02
Dimitris Kanellis <gitkanellis@gmail.com>
2015-05-21 22:23:44 +0100

    Reformat code

--- Server/src/main/java/com/github/dkanellis/fikey/FiKeyConfiguration.java
+++ Server/src/main/java/com/github/dkanellis/fikey/FiKeyConfiguration.java
@@ -1,8 +1,6 @@
 package com.github.dkanellis.fikey;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
-import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  * @author Dimitris Kanellis
--- Server/src/main/java/com/github/dkanellis/fikey/resources/FiKeyResource.java
+++ Server/src/main/java/com/github/dkanellis/fikey/resources/FiKeyResource.java
@@ -1,14 +1,8 @@
 package com.github.dkanellis.fikey.resources;
 
-import com.codahale.metrics.annotation.Timed;
-
-import com.google.common.base.Optional;
-import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author Dimitris Kanellis
